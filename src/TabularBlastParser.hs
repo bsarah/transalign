14aeec07afb48a2ef2e81a583ce0b1c1429e919d
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-02-20 17:38:47 +0100

    Fixes for blast parser

--- src/TabularBlastParser.hs
+++ src/TabularBlastParser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 -- This attoparsec module
 module TabularBlastParser (module TabularBlastParser)
@@ -9,6 +10,7 @@ import qualified Data.ByteString.Builder as S
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Vector as V
 import System.Directory
+import Data.Char
 
 -- | reads and parses tabular Blast result from provided filePath
 readTabularBlasts :: String -> IO (Either String (V.Vector BlastTabularResult))
@@ -25,13 +27,16 @@ parseTabularBlasts :: C.ByteString -> Either String (V.Vector BlastTabularResult
 parseTabularBlasts = parseOnly genParseTabularBlasts
 
 data BlastTabularResult = BlastTabularResult
-  { blastProgram :: B.ByteString,
-    blastQueryId :: B.ByteString,
-    blastQueryName :: B.ByteString,
-    blastDatabase :: B.ByteString,
-    blastHitNumber :: Int,
-    hitLines :: V.Vector BlastTabularHit
+  { blastProgram :: !BlastProgram,
+    blastQueryId :: !B.ByteString,
+    blastQueryName :: !B.ByteString,
+    blastDatabase :: !B.ByteString,
+    blastHitNumber :: !Int,
+    hitLines :: !(V.Vector BlastTabularHit)
   }
+  deriving (Show, Eq)
+
+data BlastProgram = BlastX | BlastP | BlastN
   deriving (Show, Eq)
 
 genParseTabularBlasts :: Parser (V.Vector BlastTabularResult)
@@ -39,22 +44,31 @@ genParseTabularBlasts = do
   bresults <- many1 genParseTabularBlast
   return (V.fromList bresults)
 
+genParseBlastProgram :: Parser BlastProgram
+genParseBlastProgram = do
+  choice [string "# BLAST",string "# blast"]
+  (toLower <$> anyChar) >>= return . \case 
+    'x' -> BlastX
+    'p' -> BlastP
+    'n' -> BlastN
+
 genParseTabularBlast :: Parser BlastTabularResult
 genParseTabularBlast = do
-  choice [string "# BLAST",string "# blast"]
-  _blastProgram <- many1 (notChar '\n') <?> "Program"
-  string "\n# Query: "
+  --choice [string "# BLAST",string "# blast"]
+  --_blastProgram <- choice [string "p",string "P",string "x",string "X"] <?> "Program"
+  _blastProgram <- genParseBlastProgram <?> "Program"
+  string "\n# Query: " <?> "Query"
   _blastQueryId <- many1 (notChar ' ')  <?> "QueryId"
   _blastQueryName <- many1 (notChar '\n')  <?> "QueryName"
-  string "\n# Database: "
+  string "\n# Database: " <?> "Database"
   _blastDatabase <- many1 (notChar '\n')  <?> "Db"
-  string "\n# "
+  string "\n# " <?> "header linebreak"
   --fields line
-  skipMany (try genParseFieldLine)
+  skipMany (try genParseFieldLine) <?> "Fields"
   _blastHitNumber <- decimal  <?> "Hit number"
-  string " hits found\n"
-  _tabularHit <- many' genParseBlastTabularHit
-  return $ BlastTabularResult (B.pack ("BLAST" ++ _blastProgram)) (toLB $ C.pack _blastQueryId) (toLB $ C.pack _blastQueryName) (toLB $ C.pack _blastDatabase) _blastHitNumber (V.fromList _tabularHit)
+  string " hits found\n" <?> "hits found"
+  _tabularHit <- many' (try genParseBlastTabularHit)  <?> "Tabular hit"
+  return $ BlastTabularResult _blastProgram (toLB $ C.pack _blastQueryId) (toLB $ C.pack _blastQueryName) (toLB $ C.pack _blastDatabase) _blastHitNumber (V.fromList _tabularHit)
 
 genParseFieldLine :: Parser ()
 genParseFieldLine = do
@@ -64,55 +78,55 @@ genParseFieldLine = do
   return ()
 
 data BlastTabularHit = BlastTabularHit
-  { queryId :: B.ByteString,
-    subjectId ::  B.ByteString,
-    seqIdentity :: Double,
-    alignmentLength :: Int,
-    misMatches :: Int,
-    gapOpenScore :: Int,
-    queryStart :: Int,
-    queryEnd :: Int,
-    hitSeqStart :: Int,
-    hitSeqEnd :: Int,
-    eValue :: Double,
-    bitScore :: Double,
-    subjectFrame :: Int,
-    querySeq  :: B.ByteString,
-    subjectSeq  :: B.ByteString
+  { queryId :: !B.ByteString,
+    subjectId ::  !B.ByteString,
+    seqIdentity :: !Double,
+    alignmentLength :: !Int,
+    misMatches :: !Int,
+    gapOpenScore :: !Int,
+    queryStart :: !Int,
+    queryEnd :: !Int,
+    hitSeqStart :: !Int,
+    hitSeqEnd :: !Int,
+    eValue :: !Double,
+    bitScore :: !Double,
+    subjectFrame :: !Int,
+    querySeq  :: !B.ByteString,
+    subjectSeq  :: !B.ByteString
   }
   deriving (Show, Eq)
 
 genParseBlastTabularHit :: Parser BlastTabularHit
 genParseBlastTabularHit = do
-  _queryId <- many1 (notChar '\t')
+  _queryId <- many1 (notChar '\t') <?> "hit qid"
   char '\t'
-  _subjectId <- many1 (notChar '\t')
+  _subjectId <- many1 (notChar '\t') <?> "hit sid"
   char '\t'
-  _seqIdentity <- double
+  _seqIdentity <- double <?> "hit seqid"
   char '\t'
-  _alignmentLength <- decimal
+  _alignmentLength <- decimal  <?> "hit sid"
   char '\t'
-  _misMatches <- decimal
+  _misMatches <- decimal <?> "hit mmatch"
   char '\t'
-  _gapOpenScore <- decimal
+  _gapOpenScore <- decimal <?> "hit gopen"
   char '\t'
-  _queryStart <- decimal
+  _queryStart <- decimal <?> "hit qstart"
   char '\t'
-  _queryEnd <- decimal
+  _queryEnd <- decimal  <?> "hit qend"
   char '\t'
-  _hitSeqStart <- decimal
+  _hitSeqStart <- decimal  <?> "hit sstart"
   char '\t'
-  _hitSeqEnd <- decimal
+  _hitSeqEnd <- decimal <?> "hit send"
   char '\t'
-  _eValue <- double
+  _eValue <- double <?> "hit eval"
   char '\t'
-  _bitScore <- double
+  _bitScore <- double <?> "hit bs"
   char '\t'
-  _subjectFrame <- decimal
+  _subjectFrame <- decimal <?> "hit sF"
   char '\t'
-  _querySeq <- many1 (satisfy bioLetters)
+  _querySeq <- many1 (satisfy bioLetters) <?> "hit qseq"
   char '\t'
-  _subjectSeq <- many1 (satisfy bioLetters)
+  _subjectSeq <- many1 (satisfy bioLetters) <?> "hit subSeq"
   char '\n'
   return $ BlastTabularHit (B.pack _queryId) (B.pack _subjectId) _seqIdentity _alignmentLength _misMatches _gapOpenScore _queryStart _queryEnd _hitSeqStart _hitSeqEnd _eValue _bitScore _subjectFrame (B.pack _querySeq) (B.pack _subjectSeq)
   
